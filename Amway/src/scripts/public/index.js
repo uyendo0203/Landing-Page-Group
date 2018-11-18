ed303e1b631938f14278527526d149e9d20c1712
uyendo <uyen.do@apps-cyclone.com>
2018-11-19 01:10:01 +0700

    amway

---
+++ Amway/src/scripts/public/index.js
@@ -0,0 +1,51 @@
+
+
+
+
+$(document).ready(function () {
+    initSlider();
+    AOS.init();
+});
+
+
+
+// slider------------------------------------
+function initSlider() {
+    $('.slider-1').not('.slick-initialized').slick({
+        slidesToShow: 1,
+        centerMode: true,
+        arrows:false,
+        dots:false,
+        centerPadding: '450px',
+        responsive: [
+            {
+              breakpoint: 1920,
+              settings: {
+                centerPadding: '800px',
+              }
+            },
+            {
+              breakpoint: 1801,
+              settings: {
+                centerPadding: '480px',
+              }
+            },
+            {
+              breakpoint: 1600,
+              settings: {
+                centerPadding: '320px',
+              }
+            },
+            {
+              breakpoint: 1024,
+              settings: {
+                centerPadding: '200px',
+              }
+            },
+           
+          ]
+    });
+}
+
+
+
